5970181b0e2e26e997a4cd93f02359a01cddfcf4
Alfie <AlfieABaxter@icloud.com>
2020-02-25 20:07:10 +0000

    Add an assert method

---
+++ source/debug.c
@@ -0,0 +1,19 @@
+#define Assert(expression) InvalidCode(__LINE__, __FILE__, #expression, expression, 1);
+#define SoftAssert(expression) InvalidCode(__LINE__, __FILE__, #expression, expression, 0);
+
+
+internal void
+InvalidCode(const int Line, const char * File, const char * Error, b32 result, b32 Crash) {
+    if(result) return;
+
+    char InfoLog[256];
+
+    #ifdef _WIN32
+    wsprintf(InfoLog, "Assert Failed: %s\nLine: %i File:%s\n", Error, Line, File);
+    OutputDebugStringA(InfoLog);
+    #endif
+
+    if(Crash) {
+        *((int *)0) = 0;
+    }
+}
